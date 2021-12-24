a7deec2fbace1c3bacc9626c63a32f3896d34db3
JonathanJang24 <jonathanjang24@gmail.com>
2021-12-23 20:37:30 -0600

    Added auto-scaling and updated readme

--- graph.py
+++ graph.py
@@ -1,21 +1,27 @@
 import tkinter as tk
 from tkinter.constants import CENTER, FALSE
 import time
+import math
 from tkinter.font import BOLD
 
-graph_bg,graph_color,font_name,bg_color, error_color = "#000000","#FFFFFF","PierSans-Light","#736B92", "#F88379"
+graph_bg,graph_color,font_name,bg_color, error_color = "#736B92","#F8FFE5","PierSans-Light","#736B92", "#F88379"
 
 def isNum(x):
-    return str.isdigit(x)
+    isInt = True
+    try:
+        int(x)
+    except:
+        isInt = False
+    return isInt
 
 def graphInit(eq, start, end, inc, iter):
-    global canvas, area, sqQuant
+    global canvas, area, sqQuant, lowerY, upperY
     error = tk.Label(
-                text="",
-                fg=error_color,
-                bg=bg_color,
-                font=(font_name, 16)
-            )
+            text="",
+            fg=error_color,
+            bg=bg_color,
+            font=(font_name, 16)
+        )
     error.place(relx=.5,y=520,anchor=CENTER)
     if(not(isNum(start) and isNum(end) and isNum(inc) and isNum(iter))):
         error.config(text="Error, invalid input.")
@@ -38,7 +44,7 @@ def graphInit(eq, start, end, inc, iter):
         text="",
         bg=graph_bg,
         fg=graph_color,
-        font=(font_name,20,BOLD)
+        font=(font_name,16,BOLD)
     )
     area.place(relx=.5,y=470,anchor=CENTER)
 
@@ -50,26 +56,44 @@ def graphInit(eq, start, end, inc, iter):
         fg=graph_color,
         font=(font_name,10)
     )
-    sqQuant.place(relx=.9,y=25,anchor=CENTER)
+    sqQuant.place(relx=1,y=20,anchor='e')
 
     # Upper and lower bound label
-    lower = tk.Label(
+    lowerX = tk.Label(
         graph,
         text=start,
         bg=graph_bg,
         fg=graph_color,
-        font=(font_name,12)
+        font=(font_name,10)
     )
-    lower.place(x=17, rely=.51, anchor=CENTER)
+    lowerX.place(x=0, rely=.51, anchor='w')
 
-    upper = tk.Label(
+    upperX = tk.Label(
         graph,
         text=end,
         bg=graph_bg,
         fg=graph_color,
-        font=(font_name,12)
+        font=(font_name,10)
     )
-    upper.place(x=483,rely=.51,anchor=CENTER)
+    upperX.place(relx=1,rely=.51,anchor='e')
+
+    # Upper and lower Y labels
+    lowerY = tk.Label(
+        graph,
+        text ="3",
+        bg=graph_bg,
+        fg=graph_color,
+        font=(font_name,10)
+    )
+    lowerY.place(relx=0.05,y=473,anchor=CENTER)
+    upperY = tk.Label(
+        graph,
+        text ="5",
+        bg=graph_bg,
+        fg=graph_color,
+        font=(font_name,10)
+    )
+    upperY.place(relx=.05,y=15,anchor=CENTER)
 
     summation(eq, start, end, inc, iter, 5, 0)    
         
@@ -87,6 +111,7 @@ def summation(eq, start, end, inc, iter, n, i):
     total = 0
     intX = (end-start)/n
     curr = start
+    maxY = 0
 
     # Squares for each iteration
     for j in range(n):          
@@ -95,27 +120,36 @@ def summation(eq, start, end, inc, iter, n, i):
         for k in range(len(f)):
             y += int(f[k])*pow(curr,len(f)-k-1)
         
-        vals.append((curr,y))
+        if(abs(y)>maxY):
+            maxY = abs(y)
+        vals.append((j,y))
 
         # Adding square area to total Riem. Sum
         total += y*intX
         # increment each x value
         curr += intX
 
+    lowerY.config(text="-"+str(math.ceil(maxY)))
+    upperY.config(text=str(math.ceil(maxY)))
+
     # update graph text
     area.config(text="Total Area: "+str(round(total,3)))
     sqQuant.config(text=str(n)+" squares")
 
 
-    drawSq(vals, intX, 0)
+    drawSq(vals,0,n,maxY)
     # recursivley calling the function allows for paused drawing
     if(i<iter-1):
-        canvas.after(5000,lambda:summation(eq, start, end, inc, iter, n+inc, i+1))
+        canvas.after(6000,lambda:summation(eq, start, end, inc, iter, n+inc, i+1))
 
-def drawSq(vals,inc,i):
+def drawSq(vals,i,n,maxY):
     x,y = vals[i]
-    canvas.create_rectangle((x*20,300-y,((x+inc)*20),300),fill=graph_color)
+
+    xdiv = 440/n
+    ydiv = 226/maxY
+
+    canvas.create_rectangle((30+(x*xdiv),256-(ydiv*y),(30+((x+1)*xdiv)),256),fill=graph_color)
 
     if(i<len(vals)-1):
-        canvas.after(int(5000/len(vals)),lambda:drawSq(vals,inc,i+1))
+        canvas.after(int(4500/len(vals)),lambda:drawSq(vals,i+1,n,maxY))
     
--- readme.md
+++ readme.md
@@ -11,4 +11,14 @@ This program implements a Riemann's Sum equation, evaluating the area under the 
   * Assuming Python is properly installed, open a command window and type:
     * `pip install pillow`
    
-* In a command window run 'python introScreen.py'.
+* In a command window run 'python introScreen.py'
+* After clicking through the intro screen, fill in the input fields according to the label
+* The equation currently only accepts polynomials with exponents greater than or equal to 0
+  * Simply type the coefficient before each variable to express the function
+    * Ex: 3 2 1 = 3x^2+2x+1
+* The rest of the fields require integer inputs.
+
+## Features
+* Frame by frame animation for visual learners
+* Total area for each iteration
+* Auto-scaling for custom made graph
